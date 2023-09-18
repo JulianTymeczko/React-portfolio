4f4768edbdcf86fff7b9b18d589005291a7c6f57
Julian Houle-T <jhouletymeczko@gmail.com>
2023-09-17 22:42:19 -0400

    initialized the project and successfully imported bootstrap

---
+++ react-portfo/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+function App() {
+  return (
+    <div className="card" style={{width: "18rem"}}>
+  <img src="..." class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  );
+}
+
+export default App;
